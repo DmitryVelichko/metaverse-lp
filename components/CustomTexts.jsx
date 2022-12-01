aa33a50ad85b3081bca85601b4e7ac1b40a555f9
Dmitry Velichko <soleil-2005@mail.ru>
2022-12-01 23:58:45 +0400

    Edit  About section

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,9 +1,18 @@
 'use client';
+/*eslint-disable */
 
-export const TypingText = () => (
-  <p>Typing Text</p>
+import {TypingText} from '../components'
+import { motion } from 'framer-motion';
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
+export const TypingText = ({title, textStyles}) => (
+  <motion.p
+  variants={textContainer}>
+
+  </motion.p>
 );
 
-export const TitleText = () => (
+export const TitleText = ({title, textStyles}) => (
   <h2>Title Text</h2>
 );
