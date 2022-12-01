f210d8550ee7283d0127b6b8f7ff75a642b968d6
Dmitry Velichko <soleil-2005@mail.ru>
2022-12-01 23:57:03 +0400

    Edit  About section

--- sections/About.jsx
+++ sections/About.jsx
@@ -5,11 +5,23 @@
 import {TypingText} from '../components'
 import { motion } from 'framer-motion';
 import styles from '../styles';
-import { slideIn, staggerContainer, textVariant } from '../utils/motion';
+import { fadeIn, staggerContainer } from '../utils/motion';
 
 
 const About = () => (
-  <section>
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0">
+      <motion.div
+      variants={staggerContainer}
+      initial='hidden'
+      whileInView='show'
+      viewport={{once: false, amount: 0.25}}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+      >
+        <TypingText title='| About Metaversus' textStyles='text-center'/>
+
+      </motion.div>
+    </div>
     About section
   </section>
 );
