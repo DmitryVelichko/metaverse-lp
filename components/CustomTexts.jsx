bf1724044061a1b908fdd561b7531b8099560243
Dmitry Velichko <soleil-2005@mail.ru>
2022-12-02 20:49:22 +0400

    Add Custom Texts component

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -7,8 +7,14 @@ import { textContainer } from '../utils/motion';
 
 export const TypingText = ({title, textStyles}) => (
   <motion.p
-  variants={textContainer}>
-
+  variants={textContainer}
+  className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
+  >
+{Array.from(title).map((letter, index) => (
+  <motion.span variants={textVariant2} key={index}>
+    {letter === ' ' ? '\u00A0' : letter}
+  </motion.span>
+))}
   </motion.p>
 );
 
