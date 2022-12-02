970b7ac0a7a5ee20c3c4381753025a9ce027abbb
Dmitry Velichko <soleil-2005@mail.ru>
2022-12-02 23:38:50 +0400

    Finish About section

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,23 +1,23 @@
 'use client';
 
-/*eslint-disable */
+/*eslint-disable*/
 
 import { motion } from 'framer-motion';
-import { textContainer } from '../utils/motion';
+import { textContainer, textVariant2 } from '../utils/motion';
 
-export const TypingText = ({title, textStyles}) => (
+export const TypingText = ({ title, textStyles }) => (
   <motion.p
-  variants={textContainer}
-  className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
+    variants={textContainer}
+    className={`font-normal text-[14px] text-secondary-white ${textStyles}`}
   >
-{Array.from(title).map((letter, index) => (
-  <motion.span variants={textVariant2} key={index}>
-    {letter === ' ' ? '\u00A0' : letter}
-  </motion.span>
-))}
+    {Array.from(title).map((letter, index) => (
+      <motion.span variants={textVariant2} key={index}>
+        {letter === ' ' ? '\u00A0' : letter}
+      </motion.span>
+    ))}
   </motion.p>
 );
 
-export const TitleText = ({title, textStyles}) => (
+export const TitleText = ({ title, textStyles }) => (
   <h2>Title Text</h2>
 );
