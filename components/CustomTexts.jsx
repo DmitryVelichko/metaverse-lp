b8b276f501f1f7d15092c25a2b70ebb079e48278
Dmitry Velichko <soleil-2005@mail.ru>
2022-12-02 20:23:54 +0400

    Add Custom Texts component

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,10 +1,9 @@
 'use client';
+
 /*eslint-disable */
 
-import {TypingText} from '../components'
 import { motion } from 'framer-motion';
-import styles from '../styles';
-import { fadeIn, staggerContainer } from '../utils/motion';
+import { textContainer } from '../utils/motion';
 
 export const TypingText = ({title, textStyles}) => (
   <motion.p
