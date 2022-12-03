6cdc2317e78a27a9fa682cbf3203e67f5138cfe7
Dmitry Velichko <soleil-2005@mail.ru>
2022-12-03 18:57:44 +0400

    Add Explore section

--- app/page.js
+++ app/page.js
@@ -8,19 +8,18 @@ const Page = () => (
     <div className="relative">
       <About />
       <div className="gradient-03 z-0" />
+      <Explore />
     </div>
-    <Explore />
     <div className="relative">
-    <GetStarted />
-    <div className="gradient-04 z-0" />
-
-    <WhatsNew />
+      <GetStarted />
+      <div className="gradient-04 z-0" />
+      <WhatsNew />
     </div>
     <World />
-    <div className="relative"></div>
-    <Insights />
-    <div className="gradient-04 z-0" />
-    <Feedback />
+    <div className="relative">
+      <Insights />
+      <div className="gradient-04 z-0" />
+      <Feedback />
     </div>
     <Footer />
   </div>
