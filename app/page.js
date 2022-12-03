41abf72f9e8372c596160cd0856df02c88b5acc5
Dmitry Velichko <soleil-2005@mail.ru>
2022-12-03 15:41:32 +0400

    Add gradients

--- app/page.js
+++ app/page.js
@@ -5,13 +5,23 @@ const Page = () => (
   <div className="bg-primary-black overflow-hidden">
     <Navbar />
     <Hero />
-    <About />
+    <div className="relative">
+      <About />
+      <div className="gradient-03 z-0" />
+    </div>
     <Explore />
+    <div className="relative">
     <GetStarted />
+    <div className="gradient-04 z-0" />
+
     <WhatsNew />
+    </div>
     <World />
+    <div className="relative"></div>
     <Insights />
+    <div className="gradient-04 z-0" />
     <Feedback />
+    </div>
     <Footer />
   </div>
 );
