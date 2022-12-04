809ba7ea97ae6592b51c2de69f70cfc3913fc53d
Dmitry Velichko <soleil-2005@mail.ru>
2022-12-04 22:49:50 +0400

    Explore section fixes

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -1,7 +1,5 @@
 'use client';
 
-/*eslint-disable */
-
 import { motion } from 'framer-motion';
 
 import styles from '../styles';
@@ -15,15 +13,29 @@ const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
     } flex items-center justify-center min-w-[170px] h-[700px] transition-[flex] duration-[0.7s] ease-out-flex cursor-pointer`}
     onClick={() => handleClick(id)}
   >
-    <img src={imgUrl} alt={title} className='absolute w-full h-full object-cover rounded-[24px]' />
+    <img
+      src={imgUrl}
+      alt={title}
+      className="absolute w-full h-full object-cover rounded-[24px]"
+    />
     {active !== id ? (
-      <h3 className='font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-20 lg:rotate-[-90deg] lg:origin-[0,0]'>{title}</h3>
+      <h3 className="font-semibold sm:text-[26px] text-[18px] text-white absolute z-0 lg:bottom-20 lg:rotate-[-90deg] lg:origin-[0,0]">
+        {title}
+      </h3>
     ) : (
-      <div className='absolute bottom-0 p-8 justify-end w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px] rounded-t-[24px] lg:rounded-t-[0px]'>
-        <div className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism mb-[16px]`}>
-          <img src='/headset.svg' alt="headset" className='w-1/2 h-1/2 object-contain' />
+      <div className="absolute bottom-0 p-8 justify-end w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px] rounded-t-[24px] lg:rounded-t-[0px]">
+        <div
+          className={`${styles.flexCenter} w-[60px] h-[60px] rounded-[24px] glassmorphism mb-[16px]`}
+        >
+          <img
+            src="/headset.svg"
+            alt="headset"
+            className="w-1/2 h-1/2 object-contain"
+          />
         </div>
-        <p className='font-normal text-[16px] leading-[20px] text-white uppercase'>Enter the Metaverse</p>
+        <p className="font-normal text-[16px] leading-[20px] text-white uppercase">
+          Enter the Metaverse
+        </p>
         <h2 className="mt-[24px] font-semibold sm:text-[32px] text-[24px] text-white">
           {title}
         </h2>
--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,7 +1,5 @@
 'use client';
 
-/*eslint-disable */
-
 import { useState } from 'react';
 import { motion } from 'framer-motion';
 import { ExploreCard, TitleText, TypingText } from '../components';
@@ -13,25 +11,25 @@ const Explore = () => {
   const [active, setActive] = useState('world-2');
 
   return (
-    <section className={`${styles.paddings}`} id='explore'>
+    <section className={`${styles.paddings}`} id="explore">
       <motion.div
         variants={staggerContainer}
-        initial='hidden'
-        whileInView='show'
+        initial="hidden"
+        whileInView="show"
         viewport={{ once: false, amount: 0.25 }}
         className={`${styles.innerWidth} mx-auto flex flex-col`}
       >
-        <TypingText title='| The World' textStyles='text-center' />
+        <TypingText title="| The World" textStyles="text-center" />
         <TitleText
-          title={
+          title={(
             <>
               Choose the world you want
-              <br className='md:block hidden' /> to explore{' '}
+              <br className="md:block hidden" /> to explore{' '}
             </>
-          }
-          textStyles='text-center'
+          )}
+          textStyles="text-center"
         />
-        <div className='mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5'>
+        <div className="mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5">
           {exploreWorlds.map((world, index) => (
             <ExploreCard
               key={world.id}
