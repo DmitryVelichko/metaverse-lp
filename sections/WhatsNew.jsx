5a2e2ac47e1dd6ed75b7bfdb0ba5d629187868b6
Dmitry Velichko <soleil-2005@mail.ru>
2022-12-06 00:04:46 +0400

    Add World section

--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -1,7 +1,5 @@
 'use client';
 
-/*eslint-disable */
-
 import { motion } from 'framer-motion';
 import { NewFeatures, TitleText, TypingText } from '../components';
 import styles from '../styles';
@@ -12,19 +10,19 @@ const WhatsNew = () => (
   <section className={`${styles.paddings} relative z-10`}>
     <motion.div
       variants={staggerContainer}
-      initial='hidden'
-      whileInView='show'
+      initial="hidden"
+      whileInView="show"
       viewport={{ once: 'false', amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
     >
 
       <motion.div
         variants={fadeIn('right', 'tween', 0.2, 1)}
-        className='flex-[0.75] flex justify-center flex-col'
+        className="flex-[0.75] flex justify-center flex-col"
       >
         <TypingText title="| What's New?" />
         <TitleText title={<>What's New about Metaversus?</>} />
-        <div className='mt-[48px] flex flex-wrap justify-between gap-[24px]'>
+        <div className="mt-[48px] flex flex-wrap justify-between gap-[24px]">
           {newFeatures.map((feature) => (
             <NewFeatures key={feature.title} {...feature} />
           ))}
@@ -36,9 +34,9 @@ const WhatsNew = () => (
         className={`flex-1 ${styles.flexCenter}`}
       >
         <img
-          src='/whats-new.png'
-          alt='purple planet'
-          className='w-[90%] h-[90%] object-contain'
+          src="/whats-new.png"
+          alt="purple planet"
+          className="w-[90%] h-[90%] object-contain"
         />
       </motion.div>
     </motion.div>
