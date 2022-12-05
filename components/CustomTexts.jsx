1310c25f23948155650ce1afaa2fe12b2ab9c717
Dmitry Velichko <soleil-2005@mail.ru>
2022-12-05 12:37:51 +0400

    Fix GetStarted section

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,7 +1,5 @@
 'use client';
 
-/*eslint-disable*/
-
 import { motion } from 'framer-motion';
 import { textContainer, textVariant2 } from '../utils/motion';
 
@@ -20,10 +18,10 @@ export const TypingText = ({ title, textStyles }) => (
 
 export const TitleText = ({ title, textStyles }) => (
   <motion.h2
-  variants={textVariant2}
-  initial='hidden'
-  whileInView='show'
-  className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}
+    variants={textVariant2}
+    initial="hidden"
+    whileInView="show"
+    className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}
   >
     {title}
   </motion.h2>
